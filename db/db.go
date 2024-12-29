1009c8f8c859234ee48356e867fcd7fe0bc5e694
Diego Urrego <diegourrg@gmail.com>
2024-12-29 01:35:23 -0500

    sqlite database open method added to do it in memory

--- db/db.go
+++ db/db.go
@@ -2,10 +2,30 @@ package db
 
 import (
 	"database/sql"
-	// _ "github.com/mattn/go-sqlite3"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"os"
 )
 
 func Open() (*sql.DB, error) {
-	//TO DO: IMPLEMENT
-	return nil, nil
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		log.Println("Error opening database:", err)
+		return nil, err
+	}
+
+	sqlFile, err := os.ReadFile("sql/users.sql")
+	if err != nil {
+		fmt.Println("There was an error reading users sql file", err)
+		return nil, err
+	}
+
+	_, err = db.Exec(string(sqlFile))
+	if err != nil {
+		log.Print("There was an error executing the query\n", err)
+		return nil, err
+	}
+
+	return db, nil
 }
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/goschool/crud/db"
 	"github.com/goschool/crud/routes"
+	"log"
 	"net/http"
 )
 
@@ -11,6 +13,12 @@ func main() {
 
 	r := routes.SetupRoutes()
 
+	_, err := db.Open()
+	if err != nil {
+		log.Println("There was an error in database open")
+	}
+
+	fmt.Println("Program running at port :8081")
 	http.ListenAndServe(":8081", r)
 
 	//Start the app here:
